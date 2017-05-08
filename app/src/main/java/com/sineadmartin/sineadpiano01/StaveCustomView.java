5a2e38bc1d9fc5ab25afeaa6a7412f1c14fe6a04
Frances Bawnlea <francesbawnlea@gmail.com>
2017-05-08 01:01:00 +0100

    Added methods to custom view to handle note pos, ref speed tracker Note still randomly appears on c2 button press

--- app/src/main/java/com/sineadmartin/sineadpiano01/StaveCustomView.java
+++ app/src/main/java/com/sineadmartin/sineadpiano01/StaveCustomView.java
@@ -7,13 +7,17 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.Random;
+
+import static android.R.attr.x;
 
 /**
  * Created by Sinead on 5/6/2017.
  */
 
 public class StaveCustomView extends View {
+    private int viewSize = 6;
 
     //the random circle
     public Paint mPaint;
@@ -22,10 +26,11 @@ public class StaveCustomView extends View {
     private int mPivotY = 0;
     private int radius = 20;
 
-    private int viewSize = 6;
+
     private Paint linePaint = new Paint();//To draw the white lines on black canvas
-    float[] xAxis = new float[100];//stores the x axis start point for drawing
-    float average;
+    protected static ArrayList<Float> ArrayListOfNotePositions = new ArrayList<Float>();//stores the note positions on the y axis
+    float[] yAxis = new float[12];//stores the y axis start point for drawing the note, need 12 places for division for twelve notes,stave divided downward into twelve
+    //float average;
 
     // default constructor for the class that takes in a context
     public StaveCustomView(Context c) {
@@ -52,31 +57,16 @@ public class StaveCustomView extends View {
 // constructors
     protected void init() {
 
-        linePaint.setColor(Color.WHITE);
+        linePaint.setColor(Color.WHITE);//sets the colour for drawing the lines on the stave
         //graphPaint.setColor(Color.GREEN);
         //avgPaint.setColor(Color.RED);
-        float x = (float)((getWidth()/100)+9.5);//div by 10 for 100 samples
-        for(int i = 0; i<xAxis.length;i++){
-            xAxis[i]=(float)((x*i));
+        float y = (float)((getHeight()/12)+0);//div by 10 for 100 samples
+        for(int i = 0; i<yAxis.length;i++){
+            yAxis[i]=(float)((x*i));//fill the array with the y coords for whrer to pos the notes
         }
     }
 
-    public void drawCircle() {
 
-        int minX = radius * 2;
-        int maxX = getWidth() - (radius * 2);
-
-        int minY = radius * 2;
-        int maxY = getHeight() - (radius * 2);
-
-        //Generate random numbers for x and y locations of the circle on screen
-        Random random = new Random();
-        mPivotX = random.nextInt(maxX - minX + 1) + minX;
-        mPivotY = random.nextInt(maxY - minY + 1) + minY;
-
-        //important. Refreshes the view by calling onDraw function
-        invalidate();
-    }
 
     public void onDraw(Canvas canvas) {
         canvas.drawColor(Color.BLACK);
@@ -97,6 +87,68 @@ public class StaveCustomView extends View {
 
         }
         canvas.drawCircle(mPivotX, mPivotY, radius, mPaint);
+
+        //Here maybe to start placing the nots corresponding to their positions? dunno??
+        //Or create method outside of ondraw to be called from main to place notes on button press, sounds more like it lol
+    }
+
+    public void drawCircle() {
+
+        int minX = radius * 2;
+        int maxX = getWidth() - (radius * 2);
+
+        int minY = radius * 2;
+        int maxY = getHeight() - (radius * 2);
+
+        //Generate random numbers for x and y locations of the circle on screen
+        Random random = new Random();
+        mPivotX = random.nextInt(maxX - minX + 1) + minX;
+        mPivotY = random.nextInt(maxY - minY + 1) + minY;
+
+        //important. Refreshes the view by calling onDraw function
+        invalidate();
+    }
+
+    public void drawCircleToCorrectPos(){
+        //How do I get the correct position?
+        //Is this passed in from somewhere above, the array list?
+        //Map notes to positions in the array
+        //Then can place notes depending on whcih button pressed
+    }
+
+    public void placeNoteOnStave(){//DO I NEED THIS METHOD?? MAYBE NOT!
+
+        if(!ArrayListOfNotePositions.isEmpty()){
+
+            for(int i = 0;i<ArrayListOfNotePositions.size();i++){
+                if(i==0){
+                    //cv.drawLine(xAxis[i],(float)(getHeight()*((60-ArrayListOfSpeeds.get(i))*1.666)/100),
+                          //  xAxis[i],(float)(getHeight()*((60-ArrayListOfSpeeds.get(i))*1.666)/100),graphPaint);
+
+                    //float count = 0;//needs to be in here for red line to work..??
+//            for (int i = 0; i < ArrayListOfSpeeds.size(); i++) {
+//                count += ArrayListOfSpeeds.get(i);
+//            }//make this a seperate method
+//            average = count / ArrayListOfSpeeds.size();//takes addition of all elems of array and divs by array size
+//            canvas.drawLine(0, (float) (getHeight() * ((60 - average) * 1.666) / 100), getWidth(), (float) (getHeight() * ((60 - average) * 1.666) / 100), avgPaint);
+//            MainActivity.UpdateAverageSpeed(average);
+                }
+                else{
+                    //canvas.drawLine(xAxis[i-1],(float)(getHeight()*((60-ArrayListOfSpeeds.get(i-1))*1.666)/100),
+                           // xAxis[i],(float)(getHeight()*((60-ArrayListOfSpeeds.get(i))*1.666)/100),graphPaint);
+                }
+                //drawRedLine(canvas);//pass in the canvas to this method
+            }
+//
+        }
+        else{
+            //if it is empty, do nothing?
+            //if array list is full, 100 elements? go to tracker mng class to alter?
+            //what to do?
+            //lose the least recent val and shift every element down, then add new element to end of array
+
+
+        }
     }
 
 
